5c571b0c89dd1d7932ad0392e8de08d7890910f3
Daniela <dannytorres0211@gmail.com>
2022-09-30 17:54:24 -0500

    feat: create contentSchemas and Bullets types to the custom component -- error

--- react/components/BulletGroup/index.tsx
+++ react/components/BulletGroup/index.tsx
@@ -1,10 +1,40 @@
-import React from "react"
+import React, { PropsWithChildren } from "react"
+import { BulletsSchema } from "./BulletTypes"
+import { useDevice } from "vtex.device-detector"
+import { useListContext } from "vtex.list-context"
+import { getBulletsAsTSXList } from "./modules/bulletsAsList"
 
-const BulletGroup = () => {
+export interface BulletGroupProps {
+  bullets: BulletsSchema
+}
+
+const BulletGroup = ({
+  bullets,
+  children
+}: PropsWithChildren<BulletGroupProps>
+) => {
+  const { isMobile } = useDevice();
+  const { list } = useListContext() || [];
+
+  console.log("bullets", bullets)
+
+  const bulletsContent = getBulletsAsTSXList(bullets);
+
+  if (false){
+    console.log(children, list)
+  }
   return (
-    <div>
-      Aquí tendremos un List Context!!!
-    </div>
+    <>
+    {isMobile ?
+      <div>
+        {bulletsContent}
+      </div>
+      :
+      <div>
+        Estamos en Desktop!!
+      </div>
+    }
+    </>
   )
 }
 
